427e76a7f8fccf3bb458a5c9114dd4543424567a
Anna Ponomareva <aponomareva@luxoft.com>
2021-03-03 18:26:57 +0300

    Основы React

---
+++ todo/src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import './index.css';
+
+import App from './components/app/app.js';
+
+const root = document.querySelector('#root');
+
+ReactDOM.render(<App />, root);
